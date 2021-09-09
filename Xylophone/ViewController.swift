53f6b23339fa91b19397ae6e2576068e2262f7e3
thinh <thinh@thinhs-MacBook-Pro.local>
2021-09-09 12:09:57 +0700

    init

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
-
+    var player: AVAudioPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.2
+        playSound(sender.currentTitle!)
+             DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+              //Bring's sender's opacity back up to fully opaque.
+              sender.alpha = 1.0
+          }
+        sender.alpha = 1
+    }
+    func playSound(_ path: String) {
+        let url = Bundle.main.url(forResource: path, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+         player.play()
+        
+    }
+    
     
 
 }
